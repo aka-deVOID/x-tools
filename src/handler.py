3b4c2a9e7839ab99a43aa135d42411fe3b53cc43
deVOID <dirtymbgamer@gmail.com>
2023-07-18 01:36:38 +0330

    Added Twitter handler to manage behaviors

---
+++ src/handler.py
@@ -0,0 +1,28 @@
+from selenium.webdriver import Firefox, Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from abstract import ABC
+from selenium.common.exceptions import TimeoutException
+
+
+class Twitter(ABC):
+    """
+        
+    """
+
+    def __init__(self, driver: Firefox | Chrome) -> None:
+        self.driver = driver
+
+    def load_time_out(self, second: int = 60) -> None:
+        self.driver.set_page_load_timeout(second)
+
+    def run(self) -> None:
+        try:
+            self.driver.get("https://twitter.com/")
+            self.log("info", "Request sended.")
+        except TimeoutException:
+            self.log("error", "Twitter not loaded, check internet connection or proxy setting")
+            self.driver.quit()
+        else:
+            self.log("info", "Twitter Loaded Succesfully.")
