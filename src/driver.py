a88b3ef5a7283d37e40c656044eb6160adbe0178
deVOID <dirtymbgamer@gmail.com>
2023-07-18 01:34:19 +0330

    Added log_system, Modified build

--- src/driver.py
+++ src/driver.py
@@ -1,30 +1,49 @@
-from selenium.webdriver import Chrome, Firefox
-from src.exceptions import DriverDoesNotSupported
+from selenium.webdriver import Chrome, Firefox, ChromeOptions, FirefoxOptions, FirefoxProfile
+from exceptions import DriverDoesNotSupported
+from handler import Twitter
+from typing import Self
+from abstract import ABC
+import platform
 
 
-class Driver:
-    def __init__(self, driver_name: str) -> None:
-        self.driver_options = None
-        self.driver_keep_alive = False
+class Driver(ABC):
+    def __init__(self, driver_name: str, options: FirefoxOptions | ChromeOptions = None,
+                 profile: FirefoxProfile = None) -> None:
+        self.driver_logger: str | None = None
+        self.driver_options: ChromeOptions | FirefoxOptions | None = options
+        self.driver_keep_alive: bool = False
+        self.platform: str = platform.system()
         match driver_name:
             case "chrome":
                 self.driver = Chrome
             case "firefox":
+                self.profile: FirefoxProfile = profile
                 self.driver = Firefox
             case _:
                 raise DriverDoesNotSupported(f"{driver_name} does not supported.")
 
-    def options(self) -> None:
-        """
-            TODO: Add WebDriver Options
-        """
-        pass
-
-    def keep_alive(self, toggle: bool = False):
+    def keep_alive(self, toggle: bool = False) -> Self:
         self.driver_keep_alive = toggle
+        return self
 
-    def build(self) -> Chrome | Firefox:
+    def log_system(self, toggle: bool = True) -> Self:
+        if not toggle:
+            if self.platform == "Windows":
+                self.driver_logger = "NULL"
+            else:
+                self.driver_logger = "/dev/null"
+        else:
+            self.driver_logger = None
+
+        return self
+
+    def build(self) -> Twitter:
         """
             TODO: move option to driver
         """
-        return self.driver(keep_alive=self.driver_keep_alive)
+        if self.driver_logger:
+            driver = self.driver(keep_alive=self.driver_keep_alive,
+                                 service_log_path=self.driver_logger)  # TODO: fix service_log_path not working
+        else:
+            driver = self.driver(keep_alive=self.driver_keep_alive)
+        return Twitter(driver)
