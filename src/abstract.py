2a23b0d4d408412ebfc2295cf982bf546d1c4c12
deVOID <dirtymbgamer@gmail.com>
2023-07-21 19:05:17 +0330

    Add singleton meta class

--- src/abstract.py
+++ src/abstract.py
@@ -26,6 +26,15 @@ class Logging:
                 raise LoggerException(f"{log_level} log level is not valid")
 
 
+class Singleton(type):
+    _instance = None
+
+    def __call__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instance
+
+
 class Abstract(Logging):
     """the top level of abstraction for each core class in twitter tools"""
     pass
